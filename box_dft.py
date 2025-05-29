49cd3700c2a1dd4364eb7b428f0cf0957cc801ba
le-birb <nopika99@gmail.com>
2025-05-28 18:29:11 -0700

    Set up outline

---
+++ box_dft.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+
+
+
+def kinetic_energy(density: np.ndarray) -> float:
+    return 0
+
+def ke_gradient(density: np.ndarray) -> np.ndarray:
+    return np.zeros(density.shape)
+
+def hartree_energy(density: np.ndarray) -> float:
+    return 0
+
+def hartree_gradient(density: np.ndarray) -> np.ndarray:
+    return np.zeros(density.shape)
+
+def xc_energy(density: np.ndarray) -> float:
+    return 0
+
+def xc_gradient(density: np.ndarray) -> np.ndarray:
+    return np.zeros(density.shape)
+
+energy = np.infty
+energy_tolerance = 1e-6 # or whatever
+gradient_scale = .1
+
+if __name__ == "__main__":
+    box_dims = np.array([8.0, 4.0, 1.0]) # angstroms
+    resolution = 20 # points per anstrom
+    density = np.ones(resolution*box_dims)
+    # fill in density with appropriate guess (uniform?)
+
+    while True:
+        previous_energy = energy
+        # calculate energy of configuration
+        energy = kinetic_energy(density) + hartree_energy(density) + xc_energy(density)
+
+        # check convergence
+        if abs(previous_energy - energy) < energy_tolerance:
+            break # converged
+
+        # calculate gradient
+        energy_gradient = ke_gradient(density) + hartree_gradient(density) + xc_gradient(density)
+
+        # nudge in that direction
+        density += energy_gradient * gradient_scale
+        # repeat
+    
+    print(energy)
