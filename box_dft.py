fb00c72217323ca5c96c497206f489dd6560c4af
le-birb <nopika99@gmail.com>
2025-06-02 15:50:57 -0700

    Make xc_energy more readable

--- box_dft.py
+++ box_dft.py
@@ -112,9 +112,10 @@ _a0 = .529 # angstroms
 def xc_energy(density: np.ndarray, dx: float) -> float:
     a = (np.log(2) - 1)/(2*np.pi**2)
     b = 20.4562557
-    rs_factor = (4*np.pi/3)**(1/3) / _a0
-    integrand = _lda_factor * density**(4/3) + density * a * np.log(1 + b * rs_factor * density**(-1/3) + b * rs_factor**2 * density**(-2/3))
-    return _integrate(integrand)
+    inv_rs_factor = (4*np.pi/3)**(1/3) / _a0
+    inv_rs = density**2 * inv_rs_factor
+    integrand = _lda_factor * density**(4/3) + density * a * np.log(1 + b * (inv_rs + inv_rs**2))
+    return _integrate(integrand, dx)
 
 def xc_gradient(density: np.ndarray, dx: float) -> np.ndarray:
     return np.zeros(density.shape)
