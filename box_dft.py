Fix derivative errors Positive indices don't wrap around

box_dft.py
@@ -25,9 +25,11 @@ def _grad_squared(function: np.ndarray, dx: float, dy: float = None, dz: float =
     # a symmetric differnence doesn't use the value at the point, so we don't actually need to keep it
     for _ in it:
         ix, iy, iz = it.multi_index
-        gx = (function[ix + 1, iy, iz] - function[ix - 1, iy, iz]) / (2*dx)
-        gy = (function[ix, iy + 1, iz] - function[ix, iy - 1, iz]) / (2*dy)
-        gz = (function[ix, iy, iz + 1] - function[ix, iy, iz - 1]) / (2*dz)
+        lx, ly, lz = function.shape
+        # i + 1 needs to be wrapped around explicitly, i- 1 does not
+        gx = (function[(ix + 1) % lx, iy, iz] - function[ix - 1, iy, iz]) / (2*dx)
+        gy = (function[ix, (iy + 1 % ly), iz] - function[ix, iy - 1, iz]) / (2*dy)
+        gz = (function[ix, iy, (iz + 1)] % lz - function[ix, iy, iz - 1]) / (2*dz)
         grad_squared[ix, iy, iz] = gx**2 + gy**2 + gz**2
     return grad_squared	
 
@@ -41,11 +43,12 @@ def _laplacian(function: np.ndarray, dx: float, dy: float = None, dz: float = No
     it = np.nditer(function, flags=['multi_index'])
     for value in it:
         ix, iy, iz = it.multi_index
+        lx, ly, lz = function.shape
         laplacian[ix, iy, iz] = \
             (
-                function[ix + 1, iy, iz] + function[ix - 1, iy, iz] + \
-                function[ix, iy + 1, iz] + function[ix, iy - 1, iz] + \
-                function[ix, iy, iz + 1] + function[ix, iy, iz - 1] - \
+                function[(ix + 1 % lx), iy, iz] + function[ix - 1, iy, iz] + \
+                function[ix, (iy + 1) % ly, iz] + function[ix, iy - 1, iz] + \
+                function[ix, iy, (iz + 1) % lz] + function[ix, iy, iz - 1] - \
                 6*value
             ) / dx**2 # TODO: think about adding support for different dx, dy, dz; think about it real hard
     return laplacian